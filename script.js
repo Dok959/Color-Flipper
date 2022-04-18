83e8faefd5ce4ba4a7af2270bd0af44837b41bcf
Dok959 <dok959.developer@gmail.com>
2022-04-18 11:16:14 +0700

    finish

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function change(){
-    console.log('+')
+function change() {
+    let color = '';
+
+    while (color.length < 6) {
+        let liter = Math.floor(Math.random() * 16);
+        liter = liter.toString(16);
+        color += liter;
+    }
+
+    document
+        .getElementsByTagName('body')[0]
+        .setAttribute('style', `background: #${color}`);
+
+    document.getElementsByClassName('text-color')[0].textContent =
+        color.toUpperCase();
 }
+
+// let color = window.getComputedStyle(
+//     document.getElementsByTagName('body')[0],
+//     null
+// ).backgroundColor;
+
+// document.getElementsByClassName('text-color')[0].setAttribute('value', color);
