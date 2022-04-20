80677c984e5c3e5cb5091daf110c50cbf78ef7ac
Dok959 <dok959.developer@gmail.com>
2022-04-20 11:35:48 +0700

    refactoring

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function change() {
-    let color = '';
+    let color = '#';
 
-    while (color.length < 6) {
+    while (color.length < 7) {
         let liter = Math.floor(Math.random() * 16);
         liter = liter.toString(16);
         color += liter;
     }
 
-    document
-        .getElementsByTagName('body')[0]
-        .setAttribute('style', `background: #${color}`);
+    document.body.style.backgroundColor = color;
 
     document.getElementsByClassName('text-color')[0].textContent =
         color.toUpperCase();
